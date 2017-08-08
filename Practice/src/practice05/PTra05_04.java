c1f27c28c11b59706d4eba7bb2f323cddd747e63
motoki miyashita <kinput2tcshjob@gmail.com>
2017-08-08 11:52:46 +0900

    Ptra05_01 ~ Ptra05_04まで完了

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -21,18 +21,44 @@ public class PTra05_04 {
 		 * ●0～6が入力されている場合は、各曜日を出力した後プログラムを終了します
 		 *
 		 */
+		boolean isValidNum = false;
+		while(!isValidNum) {
+			// ①
+			System.out.println("0～6の整数を入力してください");
+			String line = scanner.nextLine();
 
-		// ①
-		System.out.println("0～6の整数を入力してください");
-
-		String line = scanner.nextLine();
-
-		switch(line) {
+			switch(line) {
 			case "0":
 				System.out.println("日曜");
+				isValidNum = true;
 				break;
 			case "1":
 				System.out.println("月曜");
+				isValidNum = true;
+				break;
+			case "2":
+				System.out.println("火曜");
+				isValidNum = true;
+				break;
+			case "3":
+				System.out.println("水曜");
+				isValidNum = true;
+				break;
+			case "4":
+				System.out.println("木曜");
+				isValidNum = true;
+				break;
+			case "5":
+				System.out.println("金曜");
+				isValidNum = true;
+				break;
+			case "6":
+				System.out.println("土曜");
+				isValidNum = true;
+				break;
+			default:
+				isValidNum = false;
+			}
 		}
 	}
 }
