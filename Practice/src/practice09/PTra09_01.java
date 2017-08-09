02633fefbb4d486d352ba863e6477b342ffa2318
motoki miyashita <kinput2tcshjob@gmail.com>
2017-08-09 12:22:53 +0900

    Ptra09_01 ~ 02まで完了

--- Practice/src/practice09/PTra09_01.java
+++ Practice/src/practice09/PTra09_01.java
@@ -1,4 +1,8 @@
 package practice09;
+//import java.util.Timer;
+//import java.util.TimerTask;
+import practice09.util.Util;
+
 /*
  * PTra09_01.java
  *   作成	LIKEIT	2017
@@ -8,9 +12,31 @@ package practice09;
 
 public class PTra09_01 {
 	public static void main(String[] args) {
+
 		String str = "この文字列をゆっくり表示したい";
+		Util.dispMessageSlowly(str, 500);
 
 		// ★ 変数strを、Util内のメソッドを用いて0.5秒間隔で表示させなさい
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
 	}
 }
+
+
+/*
+public class PTra09_01 extends TimerTask{
+	public static void main(String[] args) {
+
+		Timer timer = new Timer();
+		timer.schedule(new PTra09_01(), 500, 500);
+
+
+		// ★ 変数strを、Util内のメソッドを用いて0.5秒間隔で表示させなさい
+		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
+	}
+
+	public void run() {
+		String str = "この文字列をゆっくり表示したい";
+		System.out.println(str);
+	}
+}
+*/
